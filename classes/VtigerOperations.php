17560841f52150a352e6981b0ac593a07d373696
Soslan Aldatov <me@supporteam.ru>
2015-01-27 21:12:29 +0500

    Auth is partially done

--- classes/VtigerOperations.php
+++ classes/VtigerOperations.php
@@ -1,20 +1,19 @@
 <?php
 
+ini_set('include_path', get_include_path() . PATH_SEPARATOR . dirname(__FILE__));
+require_once "Zend/Json.php";
+
 class VtigerOperations {
 
 	private $endpointUrl = "http://www.ejwelch.org/webservice.php";
 	private $username = "kchmela";
 	private $userAccessKey = "6Rk1D2ODBnHksWri";
 
-	private $httpc = '';
 
 	public function __construct() {
-		$this->httpc = new http\Client();
-
 		//authentication
-		$this->httpc->GET($endpointUrl."?operation=getchallenge&username=".$username);
-		$response = $this->httpc->currentResponse();
-		$jsonResponse = Zend_JSON::decode($response['body']);
+		$response = file_get_contents($this->endpointUrl."?operation=getchallenge&username=".$this->username);
+		$jsonResponse = Zend_JSON::decode($response);
 
 		if($jsonResponse['success']==false)
             //handle the failure case.
