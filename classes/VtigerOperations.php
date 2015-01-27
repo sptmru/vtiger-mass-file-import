f2a2c18cd73cc62a20838ec1242d0aeb5491fcb3
Soslan Aldatov <me@supporteam.ru>
2015-01-27 19:50:47 +0500

    vTiger class structure created

--- classes/VtigerOperations.php
+++ classes/VtigerOperations.php
@@ -1,5 +1,13 @@
 <?php
 
 class VtigerOperations {
-	
+
+	private $endpointUrl = "http://www.ejwelch.org/webservice.php";
+	private $username = "kchmela";
+	private $userAccessKey = "6Rk1D2ODBnHksWri";
+
+	public function __construct() {
+		//authentication
+	}
+
 }
